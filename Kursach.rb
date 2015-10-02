2330a471b54f97bbe1ec4a61ba22b19a6a7bb788
Sovunya <nastya-soba.ry@mail.ru>
2015-10-03 00:03:33 +0400

    Проблемы с кодировкой

--- Kursach.rb
+++ Kursach.rb
@@ -7,23 +7,27 @@ puts 'Ruby-скрипт для выделения текстов из стате
 # Написать скрипт, формирующий список уникальных ссылок из выдачи google.
 
 print 'Введите ваш запрос: '
-Query = gets.chomp   # проблемы с кодировкой, принимет только латиницу!!
+Query = gets.chomp   
+# проблемы с кодировкой, принимет только латиницу!!
 
 print 'Введите количество страниц: '
 N = gets.chomp.to_i
-
-# Скрипт получает  запрос (Query)  от  пользователя, отправляет  в  поисковик google
-# Выбирает неповторяющиеся ссылки с первых N  страниц поисковой  выдачи google
-doc = Nokogiri::HTML(open('http://www.google.com/search?q'+Query),'UTF-8')
-doc.css('h3.r a.l').each do |link|
-  puts link.content
-end
 
 puts 'Ваш запрос: ' + Query
 puts "Количество страниц: #{N}"
 
 puts 'Список полученных ссылок:'
-puts doc
+# Скрипт получает  запрос (Query)  от  пользователя, отправляет  в  поисковик google
+# Выбирает неповторяющиеся ссылки с первых N  страниц поисковой  выдачи google
+(1..N).each do |page_num|
+puts "Страница #{page_num}"
+url = open"http://www.google.com/search?q=#{Query}&start=#{(page_num-1)*10}"
+doc=Nokogiri::HTML url
+links=doc.css('div.s cite')
+links.each do |link|
+  puts link.content.encode("utf-8")
+end
+puts
+end
+
 # Выводит на экран список ссылок
-
-
