f1e2a275d1169afd599281e5aa7436876ff126b6
Sovunya <nastya-soba.ry@mail.ru>
2015-10-02 18:45:18 +0400

    Не правильно работает запрос googl

--- Kursach.rb
+++ Kursach.rb
@@ -1,4 +1,7 @@
-# coding: utf-8 
+# coding: utf-8
+require 'rubygems' 
+require 'nokogiri'
+require 'open-uri'
 
 puts 'Ruby-скрипт для выделения текстов из статей'
 # Написать скрипт, формирующий список уникальных ссылок из выдачи google.
@@ -11,15 +14,16 @@ N = gets.chomp.to_i
 
 # Скрипт получает  запрос (Query)  от  пользователя, отправляет  в  поисковик google
 # Выбирает неповторяющиеся ссылки с первых N  страниц поисковой  выдачи google
-
-
-
+doc = Nokogiri::HTML(open('http://www.google.com/search?q'+Query),'UTF-8')
+doc.css('h3.r a.l').each do |link|
+  puts link.content
+end
 
 puts 'Ваш запрос: ' + Query
 puts "Количество страниц: #{N}"
 
 puts 'Список полученных ссылок:'
-
+puts doc
 # Выводит на экран список ссылок
 
 
