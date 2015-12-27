ad0bd9535eba7855139dec511413ed5f93b0225a
PorubovaTatyana <tatyana.porubova@bk.ru>
2015-12-27 21:38:04 +0300

    Сохранение ссылок

--- Kursach.rb
+++ Kursach.rb
@@ -4,6 +4,8 @@ require 'nokogiri'
 require 'open-uri'
 
 puts 'Ruby-скрипт для выделения текстов из статей'
+
+# часть 1
 print 'Введите ваш запрос (Вместо пробела используйте нижнее подчеркивание): '
 Query = gets.chomp   
 # проблемы с кодировкой, принимет только латиницу!!
@@ -11,6 +13,7 @@ print 'Введите количество страниц: '
 N = gets.chomp.to_i
 puts 'Ваш запрос: ' + Query
 puts "Количество страниц: #{N}"
+
 puts 'Список полученных ссылок:'
 (1..N).each do |page_num|
 puts "Страница #{page_num}"
@@ -18,8 +21,11 @@ url = open"http://www.google.com/search?q=#{Query}&start=#{(page_num-1)*10}"
 doc=Nokogiri::HTML url
 links=doc.css('cite')
 links.each do |link|
-  puts link.text.encode("utf-8")
-  # Если ссылки длинные выводит их с многоточием.
+	puts link.text.encode("utf-8")
+	ad='c:/MMAD/links_file.txt' 							# сохранение ссылок в файл 
+	File.open(ad, 'a'){ |f| f.write("#{link.text}\n") }
 end
 puts
 end
+
+# часть 2 
